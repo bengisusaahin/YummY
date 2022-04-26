a2141c725bb8d3ce46382b8d9d6de12d1e38700f
Bengisu Şahin <bengisusaahin@gmail.com>
2022-04-26 17:03:26 +0300

    Uploaded backend

---
+++ backend/Domain/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public int? ParentDepartmentId { get; set; }
+    }
+}
