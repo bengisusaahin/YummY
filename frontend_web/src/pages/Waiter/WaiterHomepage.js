f49607a66ed5b1aab7990bc33dc644b0940b5ead
Bengisu Şahin <bengisusaahin@gmail.com>
2022-05-27 22:36:58 +0300

    Added WaiterHomepage

--- frontend_web/src/pages/Waiter/WaiterHomepage.js
+++ frontend_web/src/pages/Waiter/WaiterHomepage.js
@@ -1,8 +1,123 @@
-import React from 'react';
+import React from "react";
+import classes from "./WaiterHomepage.module.css";
 
 const WaiterHomepage = () => {
   return (
-    <div>WaiterHomepage</div>
+    <>
+      <div className={classes.tablesKnowledge}>
+        <div className={classes.tableKnowledge}>Empty Tables</div>
+        <div className={classes.tableKnowledge}>Non-Empty Tables</div>
+        <div className={classes.tableKnowledge}>Ready to Serve Tables</div>
+      </div>
+
+      <div className={classes.tables}>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>2</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>3</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>4</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>4</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+        <div className={classes.tableOuterDiv}>
+          <div className={classes.tableItem}>1</div>
+        </div>
+      </div>
+    </>
   );
 };
 
